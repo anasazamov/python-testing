6f8f395fcfeae340ea9d547b435b3e546430313d
djumanov <diyorbekjumanov1120@gmail.com>
2023-06-22 16:28:43 +0500

    init

---
+++ test_main.py
@@ -0,0 +1,18 @@
+def main():
+    return "Hello World."
+
+
+def test_main_01():
+    output = main()
+
+    answer = "Hello World."
+    assert output == answer, "wrong answer"
+
+
+
+def test_main_02():
+    output = main()
+
+    answer = "Hello World."
+    assert output == answer, "wrong answer"
+
